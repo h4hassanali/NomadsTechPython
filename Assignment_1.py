e44509de4dff77e1db4396dc36f4a08836c9f15c
h4hassanali <hxn112233@gmail.com>
2024-10-10 19:37:14 +0500

    Removed the global

--- Assignment_1.py
+++ Assignment_1.py
@@ -2,20 +2,20 @@ from sys import argv
 from time import sleep
 import keyboard
 
-def on_space_press(event):
-    """ this function is callback function, which turns
-    space button to 'True' when Space key is pressed """
-    global space_button
-    space_button = True
+def on_space_press(event, state):
+    """ this function is callback function, which updates 
+    the state to True when Space key is pressed """
+    state['space_button'] = True
 
 def run_counter(num_sec):
-    keyboard.on_press_key('space', on_space_press)
+    space_button_state = {'space_button': False}
+    keyboard.on_press_key('space', lambda event: on_space_press(event, space_button_state))
+    
     for i in range(num_sec):
-        if space_button:
+        if space_button_state['space_button']:
             break
         print(i)
         sleep(1)
 
-space_button = False
 seconds = int(argv[1])
 run_counter(seconds)
