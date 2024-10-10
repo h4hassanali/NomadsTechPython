debd87fd94b4ce334e5af2bc75fe5380f99e0c5a
h4hassanali <hxn112233@gmail.com>
2024-10-10 19:50:52 +0500

    used argparse library for named command line arguments.

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,21 +1,25 @@
-from sys import argv
+import argparse
 from time import sleep
 import keyboard
 
 def on_space_press(event, state):
-    """ this function is callback function, which updates 
-    the state to True when Space key is pressed """
+    """This function updates the state to True when Space key is pressed"""
     state['space_button'] = True
 
 def run_counter(num_sec):
     space_button_state = {'space_button': False}
     keyboard.on_press_key('space', lambda event: on_space_press(event, space_button_state))
-    
     for i in range(num_sec):
         if space_button_state['space_button']:
             break
         print(i)
         sleep(1)
 
-seconds = int(argv[1])
-run_counter(seconds)
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--target', type=int, required=True)
+        args = parser.parse_args()
+        if args.target <= 0:
+            raise ValueError("The --target value must be a positive integer.")
+        run_counter(args.target)
+
